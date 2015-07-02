512b26362ceb4736a415348a4dab4d8c84b31034
brenuuuh <brenuhfigueiredo@gmail.com>
2015-07-02 14:28:14 -0300

    Prototipo Final do Projeto Sem CRUD da Agenda

---
+++ SalaoNaWeb/Controllers/EventController.cs
@@ -0,0 +1,36 @@
+﻿using SalaoNaWeb.Migrations;
+using SalaoNaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalaoNaWeb.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        private Contexto db = new Contexto();
+
+        // GET api/Event
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public IQueryable GetEvents()
+        {
+            return db.Events;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SalaoNaWeb/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalaoNaWeb.Models
+{
+    public class Event
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+}
